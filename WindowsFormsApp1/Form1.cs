4b9606a0eaf0be86f26d43bf1e26e1bac61e3f61
Kj kijiramer <kijiramer@hotmail.fr>
2019-04-14 05:08:09 +0200

    Premier push (login form et connection avec la base de donnée 'check')

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection connection = new OleDbConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\kijiramer\Desktop\M2L-Projet-FDERI\frais.mdb";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickk(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                checkConnection.Text = "Connection Successful";
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error "+ ex);
+            }
+        }
+
+        
+    }
+}
